d94a6848c3695105647ca457cf0c6b45c7c83ad6
Juanita Vyatri <juanita.vyatri@gmail.com>
2018-11-24 08:22:21 +0700

    Initial Commit

---
+++ Image Machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Machine
+//
+//  Created by Juanita Vyatri on 24/11/18.
+//  Copyright © 2018 Vyatri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
